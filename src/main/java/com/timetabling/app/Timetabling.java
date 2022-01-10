059ab23b2af7d3f0e4aa107c1bd4d90d9b29bb74
dritelinda.hajdari <dritelinda.hajdari@asseco-see.com>
2022-01-10 23:13:39 +0100

    Phase 3

--- src/main/java/com/timetabling/app/Timetabling.java
+++ src/main/java/com/timetabling/app/Timetabling.java
@@ -2,12 +2,15 @@ package com.timetabling.app;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 import org.json.simple.parser.ParseException;
@@ -28,41 +31,102 @@ import com.timetabling.app.util.JSONUtils;
 public class Timetabling {
 	
 	private static Map<Period, List<Room>> periodRoomRelation = new HashMap<Period, List<Room>>();
-	private static BigDecimal cost = BigDecimal.ZERO;
+	private static int cost = 0;
+	private static Solution best = null;
 	
 	public static void main(String[] args) throws ParseException {
-		List<Assignment> assignments = new ArrayList<>();
 		Instance inst =  JSONUtils.convert(JSONUtils.getFileData("D1-1-16.json"), new TypeReference<Instance>(){});
-		//Map<Course, List<Room>> courseRoomRelation = new HashMap<Course, List<Room>>();
 		List<Course> courses = inst.getCourses();
 		List<Room> rooms = inst.getRooms();
 		List<Curricula> curriculas = inst.getCurricula();
-		List<Exam> exams = new ArrayList<Exam>();
-		periodRoomRelation = getPeriods(inst.getPeriods(), inst.getSlotsPerDay(), rooms);
-		for (int i = 0; i < 10; i++) {
-			curriculas.stream().forEach(curricula -> {
-				List<String> primaryCourses = curricula.getPrimaryCourses();
-				List<String> secondaryCourses = curricula.getSecondaryCourses();
-				checkingConstraints(inst, courses, exams, curricula, primaryCourses, assignments);
-				checkingConstraints(inst, courses, exams, curricula, secondaryCourses, assignments);
-			});
-			Solution solution = new Solution.Builder().assignments(assignments).cost(cost).build();
-			cost = BigDecimal.ZERO;
-			JSONUtils.saveFile(JSONUtils.convert(solution), "Assignments.json");
-			System.out.println(JSONUtils.convert(solution));
-			solution.mutation();
+		//generate componenets, evapuration constant (0 < x < 1), phoremons 
+		Random rand = new Random(); 
+		int evaporation = rand.nextInt(1);
+		int initializePheromoneValue = 0;
+		int numberOfTrails = 10;
+		int[] pheromones = initializePheromones(numberOfTrails, initializePheromoneValue);
+		int[] components = generateDummyComponents();
+		int repeat = 0;
+		while(repeat < 5) {
+			List<Solution> solutions = new ArrayList<Solution>();
+			for (int i = 0; i < 10; i++) {
+				int[] componentsOfSolution = getRandomSolutionComponents(components);
+				List<Assignment> assignments = new ArrayList<>();
+				List<Exam> exams = new ArrayList<Exam>();
+				periodRoomRelation = getPeriods(inst.getPeriods(), inst.getSlotsPerDay(), rooms);
+				curriculas.stream().forEach(curricula -> {
+					List<String> primaryCourses = curricula.getPrimaryCourses();
+					List<String> secondaryCourses = curricula.getSecondaryCourses();
+					checkingConstraints(inst, courses, exams, curricula, primaryCourses, assignments, inst.getPrimaryPrimaryDistance());
+					checkingConstraints(inst, courses, exams, curricula, secondaryCourses, assignments, inst.getPrimarySecondaryDistance());
+				});
+				Solution solution = new Solution.Builder().assignments(assignments).cost(cost).components(componentsOfSolution).build();
+				//check components and best solution 
+				
+				System.out.println(JSONUtils.convert(solution));
+				if(!assignments.isEmpty()) {
+					solution.mutation(inst);
+				}
+				if(best == null) {
+					best = solution;
+				} else if (best.getCost() <= solution.getCost()) {
+					best = solution;
+				}
+				solutions.add(solution);
+				for (int j = 0; j < pheromones.length; j++) {
+					pheromones[i] = (1 - evaporation) * pheromones[i];
+				}
+				for (int j = 0; j < solutions.size(); j++) {
+					int[] componentsOfCurrentSolution = solutions.get(j).getComponents();
+					for (int j2 = 0; j2 < components.length; j2++) {
+						if(contains(componentsOfCurrentSolution, components[j2])) {
+							pheromones[j2] = pheromones[j2] + solutions.get(j).getCost();
+						}
+					}
+				}
+				cost = 0;
+			}
+			repeat++;
 		}
-		
+		JSONUtils.saveFile(JSONUtils.convert(best), "Assignments.json");
+	}
+	
+	 public static boolean contains(final int[] arr, final int key) {
+	        return Arrays.stream(arr).anyMatch(i -> i == key);
+	    }
+
+	private static int[] generateDummyComponents() {
+		Random rand = new Random(); 
+		return new int[] {rand.nextInt(25),rand.nextInt(25),rand.nextInt(25),rand.nextInt(25),rand.nextInt(25)};
+	}
+	
+	private static int[] initializePheromones(int numberOfTails, int initializePheromoneValue) {
+		int[] pheromones = new int[numberOfTails];
+		for (int i = 0; i < pheromones.length; i++) {
+			pheromones[i] = initializePheromoneValue;
+		}
+		return pheromones;
+	}
+	
+	private static int[] getRandomSolutionComponents(int [] components) {
+		Random rand = new Random(); 
+		List<Integer> intList = new ArrayList<Integer>(components.length);
+		for (int i : components)
+		{
+		    intList.add(i);
+		}
+		Collections.shuffle(intList);
+		List<Integer> comp = intList.subList(0,rand.nextInt(components.length));
+		return comp.stream().mapToInt(Integer::intValue).toArray();
 	}
 
 	private static List<Assignment> checkingConstraints(Instance inst, List<Course> courses, List<Exam> exams, Curricula curricula,
-			List<String> curricumCourses, List<Assignment> assignments) {
+			List<String> curricumCourses, List<Assignment> assignments, BigDecimal distance) {
 		Collections.shuffle(curricumCourses);
 			for (String prCourse : curricumCourses) {
 				Optional<Course> course = courses.stream().filter(cr -> cr.getCourse().equalsIgnoreCase(prCourse)).findAny();
-				Entry<Period, List<Room>> periodAndCourseRoom = getRequestedCourseRooms(course.get(), exams, inst);
+				Entry<Period, List<Room>> periodAndCourseRoom = getRequestedCourseRooms(course.get(), exams, inst, curricula, distance ,curricumCourses);
 				if(periodAndCourseRoom != null) {
-					cost = cost.add(new BigDecimal(10));
 					List<Room> selectedPeriod = periodAndCourseRoom.getValue().stream().filter(r -> r.getType().equals(course.get().getRoomsRequested().getType())).limit(course.get().getRoomsRequested().getNumber()).collect(Collectors.toList());
 					Exam exam = new Exam.Builder().course(course.get()).rooms(selectedPeriod)
 							.period(periodAndCourseRoom.getKey()).curriculum(curricula.getCurriculum()).build();
@@ -94,7 +158,7 @@ public class Timetabling {
 		return periodRooms;
 	}
 	
-	public static Entry<Period, List<Room>> getRequestedCourseRooms(Course course, List<Exam> exams, Instance inst) {
+	public static Entry<Period, List<Room>> getRequestedCourseRooms(Course course, List<Exam> exams, Instance inst, Curricula curricula, BigDecimal distance, List<String> courses) {
 		 List<Room> rooms = inst.getRooms().stream().filter(room -> room.getType().equals(course.getRoomsRequested().getType()))
 				.limit(course.getRoomsRequested().getNumber()).collect(Collectors.toList());
 		 Map<Period, List<Room>> periodOfRooms = periodRoomRelation.entrySet().stream().filter(e -> CollectionUtils.containsAny(e.getValue(), rooms))
@@ -102,10 +166,47 @@ public class Timetabling {
 		 if(exams.isEmpty()) {
 			 return periodOfRooms.entrySet().iterator().next();
 		 } else {
+			Entry<Period, List<Room>> softConstraintRoomPeriod = checkPeriodSoftConstraint(periodOfRooms, exams, curricula, course, courses, distance);
+			if(softConstraintRoomPeriod != null) {
+				return softConstraintRoomPeriod;
+			}
 			Optional<Entry<Period, List<Room>>> periodOfRoomSelected = periodOfRooms.entrySet().stream().filter(
 					e -> exams.stream().anyMatch(ex -> !ex.getCourse().getTeacher().equals(course.getTeacher()) && !ex.getPeriod().equals(e.getKey())))
 					.findFirst();
 			return periodOfRoomSelected.isPresent() ? periodOfRoomSelected.get() : null;
 		}
+	}
+	
+	public static Entry<Period, List<Room>> checkPeriodSoftConstraint(Map<Period, List<Room>> periodOfRooms, List<Exam> exams, Curricula curricula, Course course, List<String> courses, BigDecimal distance) {
+		List<String> allCurriculumCourses = new ArrayList<String>(curricula.getPrimaryCourses());
+		allCurriculumCourses.addAll(curricula.getSecondaryCourses());
+		List<Exam> addedCurriculumCourses = exams.stream().filter(exam -> allCurriculumCourses.contains(exam.getCourse().getCourse())).collect(Collectors.toList());
+		Map<Period, List<Room>> availablePeriodOfRooms = periodOfRooms.entrySet().stream().filter(
+				e -> addedCurriculumCourses.stream().anyMatch(ex -> !ex.getCourse().getTeacher().equals(course.getTeacher()) && !ex.getPeriod().equals(e.getKey())))
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+		Entry<Period, List<Room>> periodOfRoom = checkCourseDistanceSoftConstraint(availablePeriodOfRooms, exams, courses, course, distance);
+		if(periodOfRoom != null) {
+			cost = cost + 2;
+			return periodOfRoom;
+		}
+		Optional<Entry<Period, List<Room>>> periodOfRoomSelected = periodOfRooms.entrySet().stream().filter(
+				e -> addedCurriculumCourses.stream().anyMatch(ex -> !ex.getCourse().getTeacher().equals(course.getTeacher()) && !ex.getPeriod().equals(e.getKey())))
+				.findFirst();
+		if(periodOfRoomSelected.isPresent()) {
+			cost = cost + 1;
+			return periodOfRoomSelected.get();
+		}
+		return null;
+	}
+	
+	public static Entry<Period, List<Room>> checkCourseDistanceSoftConstraint(Map<Period, List<Room>> periodOfRooms, List<Exam> exams, List<String> courses, Course course, BigDecimal distance) {
+		List<Exam> addedCourses = exams.stream().filter(exam -> courses.contains(exam.getCourse().getCourse())).collect(Collectors.toList());
+		Collections.sort(addedCourses, Comparator.comparing(o -> ((Exam) o).getPeriod().getDay()).reversed());
+		if(addedCourses.isEmpty()) {
+			return null;
+		}
+		BigDecimal lastAddedExamDayPeriod = new BigDecimal (addedCourses.get(0).getPeriod().getDay());
+		Optional<Entry<Period, List<Room>>> periodOfRoomSelected = periodOfRooms.entrySet().stream().filter(e -> new BigDecimal(e.getKey().getDay()).compareTo(lastAddedExamDayPeriod.add(distance)) > 1 ).findFirst();
+		return periodOfRoomSelected.isPresent() ? periodOfRoomSelected.get() : null;
 	}
 }
--- src/main/java/com/timetabling/app/model/Solution.java
+++ src/main/java/com/timetabling/app/model/Solution.java
@@ -1,13 +1,16 @@
 package com.timetabling.app.model;
 
-import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Solution {
 	private Random randomGenerator;
 	private List<Assignment> assignments;
-	private BigDecimal cost;
+	private int cost;
+	private int[] components; 
 
 	public List<Assignment> getAssignment() {
 		return assignments;
@@ -17,29 +20,44 @@ public class Solution {
 		this.assignments = assignments;
 	}
 	
-	public BigDecimal getCost() {
+	public int getCost() {
 		return cost;
 	}
-	public void setCost(BigDecimal cost) {
+	public void setCost(int cost) {
 		this.cost = cost;
-	}
+	} 
 	
+	public int[] getComponents() {
+		return components;
+	}
+
+	public void setComponents(int[] components) {
+		this.components = components;
+	}
+
 	public Solution(Builder builder) {
 		this.assignments = builder.assignments;
 		this.cost = builder.cost;
+		this.components = builder.components;
 	}
 	
 	public static class Builder {
 		private List<Assignment> assignments;
-		private BigDecimal cost;
+		private int cost;
+		private int[] components;
 		
 		public Builder assignments(List<Assignment> assignments) {
 			this.assignments = assignments;
 			return this;
 		}
 		
-		public Builder cost(BigDecimal cost) {
+		public Builder cost(int cost) {
 			this.cost = cost;
+			return this;
+		}
+		
+		public Builder components(int [] components) {
+			this.components = components;
 			return this;
 		}
 		
@@ -48,7 +66,7 @@ public class Solution {
 		}
 	}
 	
-	public void mutation() {
+	public void swap() {
 		randomGenerator = new Random();
 		Assignment firstAssignment = this.assignments.get(randomGenerator.nextInt(this.assignments.size()));
 		Assignment secondAssignment = this.assignments.get(randomGenerator.nextInt(this.assignments.size()));
@@ -63,6 +81,21 @@ public class Solution {
 			secondEvents.remove(0);
 			firstEvents.add(firstEvent);
 			secondEvents.add(secondEvent);
+			this.setAssignment(assignments);
+		}
+	}
+	
+	public void mutation(Instance inst) {
+		randomGenerator = new Random();
+		Assignment firstAssignment = this.assignments.get(randomGenerator.nextInt(this.assignments.size()));
+		Event firstEvent = firstAssignment.getEvents().get(0);
+		if(firstEvent != null) {
+			Optional<Room> room = inst.getRooms().stream().filter(rt -> rt.getRoom().equals(firstEvent.getRoom())).findAny();
+			if(room.isPresent()) {
+				List<Room> rooms = inst.getRooms().stream().filter(r -> r.getType().equals(room.get().getType())).collect(Collectors.toList());
+				Collections.shuffle(rooms);
+				firstEvent.setRoom(rooms.get(0).getRoom());
+			}
 			this.setAssignment(assignments);
 		}
 	}
